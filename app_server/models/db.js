9533b3f263a73942397087252152483ee1c9be0d
sevvalbadilli <sevvalbadilli0@gmail.com>
2022-11-10 19:33:32 +0300

    update1

---
+++ app_server/models/db.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose' );
+require("./mekansema"); 
+var dbURI = 'mongodb://localhost/mekanbul';
+mongoose.connect(dbURI);
+
+function kapat(msg,callback){
+mongoose.connection.close(function(){
+    console.log(msg);
+    callback();
+});
+}
+process.on("SIGINT", function () {
+    kapat("uygulama kapatıldı", function(){
+        process.exit(0);
+    });
+});
+
+
+mongoose.connection.on("conected", function(){
+    console.log(dbURI + "adresindeki veritabanına bağlanıldı\n");
+});
+mongoose.connection.on("error", function(){
+    console.log("Baglantı hatası\n");
+});
+mongoose.connection.on("disconnected", function(){
+    console.log("baglantı kesildi\n");
+});
