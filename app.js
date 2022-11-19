60112da5756a881e8acd13e4bdf88a26068ed3a7
sevvalbadilli <sevvalbadilli0@gmail.com>
2022-11-19 20:00:10 +0300

    odev5

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
-require('./app_server/models/db');
+require('./app_api/models/db');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var apiRouter = require('./app_api/routes/index');
 
 var app = express();
+app.use("/api",apiRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
